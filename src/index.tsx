a70676f78c76ac7f280d140e3e92a59788c741df
stailzgit <77897064+stailzgit@users.noreply.github.com>
2022-05-01 23:23:24 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import {store} from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { store } from "./store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
 );
-
